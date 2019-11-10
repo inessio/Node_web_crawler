bf56939a70cb43b0e06c0ca42388958d69ef0971
komlan Sodji <duhamelgking@gmail.com>
2019-11-10 08:01:41 +0000

    update and improvement on crawler.js

--- index.js
+++ index.js
@@ -2,11 +2,8 @@
 
 import express from 'express'
 import bodyParser from 'body-parser'
-import crawler from './src/crawler'
+import crawl  from './src/crawler'
 import { check, validationResult } from 'express-validator'
-
-
-
 
 
 const app = express()
@@ -14,19 +11,23 @@ const app = express()
 app.use(bodyParser.json())
 
 app.post('/crawl',[
-    check('head').isURL()
+    check('domain').isURL()
     
 ], (req,res) => {
-
+    // console.log(req.body.domain,req.body.regexes)
+    const request = req.body
+    console.log('request', request)
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
+    crawl(request.domain,request.numLevels,request.regexes)
+    // crawler;
     return res.json({
         data:"welcome"
     })
 })
 
-app.listen(8080, ()=> {console.log('hello')})
+app.listen(8090, ()=> {console.log('hello')})
 
 
--- src/crawler.js
+++ src/crawler.js
@@ -1,20 +1,34 @@
 'use strict'
 
-import URL from 'url-parse'
-import cheerio from 'cheerio'
+import URL from 'url-parse';
+import cheerio from 'cheerio';
 
 
+var pagesToVisit = [];
+var numPagesVisited = 0;
+var pagesVisited = new Set(); 
 
-const crawler = {
-
-    crawl: (domain, numLevels = 3,regexes = []) => {
-        
-    }
+async function crawl(domain, numLevels = 3,regexes = []){
+  await pagesToVisit.push(domain);
+  if(numPagesVisited >= numLevels) {
+    console.log("Reached max limit of number of pages to visit.");
+    return;
+  }
+  var nextPage = pagesToVisit.pop();
+  if (pagesVisited.has(nextPage)) {
+      // We've already visited this page, so repeat the crawl
+      crawl();
+  } else {
+      // New page we haven't visited
+      console.log('we are here')
+      // visitPage(nextPage, crawl);
+  }
 }
 
-
-const visitPage = () => {
-    console.log("im hungry")
+const visitPage = (url,callback) => {
+    pagesVisited.add(url);
+    numPagesVisited++
+    console.log("Visiting page " + url);
 }
 
-export default crawler
+export default crawl
